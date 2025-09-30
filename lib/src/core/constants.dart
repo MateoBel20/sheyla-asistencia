322e346173927655f18d381ad59219c32f0980d8
Mateo Beltrán <105222194+MateoBel20@users.noreply.github.com>
2025-09-30 09:50:27 -0500

    Variables de Conexiones
    Se incorporaron las variables de conexión para conectar con AppWrite.

---
+++ lib/src/core/constants.dart
@@ -0,0 +1,4 @@
+const String APPWRITE_ENDPOINT = 'https://nyc.cloud.appwrite.io/v1';
+const String APPWRITE_PROJECT_ID = '68dbe1fd0027e2fd7da3';
+const String APPWRITE_DATABASE_ID = '68dbe22800053433bd79';
+const String APPWRITE_COLLECTION_ID = '123456789msbb';
